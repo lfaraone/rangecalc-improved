6f978ec1ac2fdb827ba6a3dc274526477e2ec21e
Luke Faraone <luke@faraone.cc>
2014-08-08 23:05:54 -0400

    Read SECRET_KEY from a file

--- rangecalc/settings.py
+++ rangecalc/settings.py
@@ -16,8 +16,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oksb0_#kj4frab1knhe9@7otu%p3(u1rk&0%ld5%l7msdv9lqk'
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
